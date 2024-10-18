96218b927a83212621992f3d6b37416acaee6f2e
DanaPar <danaprn@gmail.com>
2024-10-18 06:29:08 +0300

    made changes, removed commetns, added system('pause'); so program doesnt closes immediately

--- Laboratorijas_Darbs_N1_solution/Laboratorijas_Darbs_N1_project/main.cpp
+++ Laboratorijas_Darbs_N1_solution/Laboratorijas_Darbs_N1_project/main.cpp
@@ -3,16 +3,14 @@
 
 int main()
 {
-	//declaring variables
 	int alpha;
-	float z1, z2;
-	//gather inputs
+	double z1, z2;
 	printf("Input radians: ");
 	scanf_s("%i", &alpha);
-	//do math
 	z1 = 1 - (1.0 / 4.0) * pow(sin(2 * alpha), 2) + cos(2 * alpha);
 	z2 = pow(cos(alpha), 2) + pow(cos(alpha), 4);
-	//show output
-	printf("%f\n", z1);
-	printf("%f", z2);
+	printf("z1 = %f\n", z1);
+	printf("z2 = %f\n\n", z2);
+	system("pause");
+	return 0;
 }
